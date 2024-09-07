253fd8c191e437f279a48fb981b77893cd3f9cd6
Knemaire <kannemaire@gmail.com>
2024-09-07 19:32:59 -0400

    init

--- exercises.py
+++ exercises.py
@@ -62,11 +62,107 @@
 #
 # Define the function and then call it below.
 
-def convert_temperature(t, u):
-    if u == 'C':
-        return (t * 9/5) + 32
-    elif u == 'F':
-        return (t - 32) * 5/9
+# def convert_temperature(t, u):
+#     if u == 'C':
+#         return (t * 9/5) + 32
+#     elif u == 'F':
+#         return (t - 32) * 5/9
+#     else:
+#         return('invalid entry')
 
-print('Exercise 4: Convert 0°C to Fahrenheit:', convert_temperature(0, 'C'))
-print('Exercise 4: Convert 32°F to Celsius:', convert_temperature(32, 'F'))
+# print('Exercise 4: Convert 0°C to Fahrenheit:', convert_temperature(0, 'C'))
+# print('Exercise 4: Convert 32°F to Celsius:', convert_temperature(32, 'F'))
+
+# Exercise 5: Sum to N
+#
+# Write a function named `sum_to` that takes a single integer n and returns the sum of all integers from 1 to n.
+#
+# Examples:
+# sum_to(6) should return 21.
+# sum_to(10) should return 55.
+#
+# Define the function and then call it below.
+# def sum_to(n):
+#     return n/2 * (1 + n)
+
+
+# print('Exercise 5:', sum_to(6))
+
+# Exercise 6: Find the Largest Number
+#
+# Write a function named `largest` that takes three integers as arguments and returns the largest of them.
+#
+# Examples:
+# largest(1, 2, 3) should return 3.
+# largest(10, 4, 2) should return 10.
+#
+# Define your function and test it with different inputs.
+# def largest(a, b, c):
+#     if a > b and a > c:
+#         return a
+#     elif b > a and b > c:
+#         return b
+#     elif c > b and c > a:
+#         return c
+# print('Exercise 6:', largest(1, 2, 3))
+
+# Exercise 7: Calculate a Tip
+#
+# Create a function called `calculate_tip`. It should take the bill amount and the tip percentage (as a whole number).
+# The function should return the amount of the tip.
+#
+# Examples:
+# calculate_tip(50, 20) should return 10.
+#
+# Write your function and test its output below.
+# def calculate_tip(b, p):
+#     return b * (p/100)
+
+
+
+# print('Exercise 7:', calculate_tip(50, 20))
+
+# Exercise 8: Calculate Product of Numbers
+#
+# Write a function named `product` that takes an arbitrary number of numbers, multiplies them, and returns the product.
+# Review your notes on *args for handling an arbitrary number of arguments.
+#
+# Examples:
+# product(-1, 4) should return -4.
+# product(2, 5, 5) should return 50.
+#
+# Define the function and call it with different sets of numbers to test.
+# def product(*nu):
+#     result = 1
+#     for n in nu:
+#         result *= n
+#     return result
+
+
+# print('Exercise 8:', product(2, 5, 5))
+
+# Exercise 9: Basic Calculator
+#
+# Create a function named `basicCalculator` that takes three arguments: 
+# two numbers and a string representing an operation ('add', 'subtract', 'multiply', 'divide'). 
+# Perform the provided operation on the two numbers. In operations where the order of numbers is important, 
+# treat the first parameter as the first operand and the second parameter as the second operand.
+#
+# Examples:
+# basicCalculator(10, 5, 'subtract') should return 5.
+# basicCalculator(10, 5, 'add') should return 15.
+# basicCalculator(10, 5, 'multiply') should return 50.
+# basicCalculator(10, 5, 'divide') should return 2.
+#
+# Define the function and then call it below.
+def basicCalculator(n1, n2, o):
+    if o == 'add':
+        return n1 + n2
+    elif o == 'subtract':
+        return n1 - n2
+    elif o == 'multiply':
+        return n1 * n2
+    elif o == 'divide':
+        return n1/n2
+
+print('Exercise 9 Result:', basicCalculator(10, 5, "subtract"))
